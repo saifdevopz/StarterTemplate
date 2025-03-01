914270fc3ccc6c1d5296423416b74e88c1392087
saifdevopz <saifdevopz@gmail.com>
2025-03-01 03:57:04 +0200

    Changed tags

--- src/modules/system/System.Presentation/Common/Tags.cs
+++ src/modules/system/System.Presentation/Common/Tags.cs
@@ -1,7 +1,7 @@
 ﻿namespace System.Presentation.Common;
 internal static class Tags
 {
-    internal const string Token = "TokensNaruto";
+    internal const string Token = "TokensDeathNote";
     internal const string TenantType = "TenantTypes";
     internal const string Tenant = "Tenants";
     internal const string User = "Users";
