fe7d460d0205b0a9b11411275c1b1481a8937b3f
saifdevopz <saifdevopz@gmail.com>
2025-03-08 05:32:13 +0200

    indexed update

---
+++ src/apps/blazor-wasm-pwa/BlazorWasmTemplate/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorWasmTemplate.Models;
+
+internal class Person
+{
+    public long? Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+}
--- src/apps/blazor-wasm-pwa/BlazorWasmTemplate/Program.cs
+++ src/apps/blazor-wasm-pwa/BlazorWasmTemplate/Program.cs
@@ -1,10 +1,35 @@
 using BlazorWasmTemplate;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using TG.Blazor.IndexedDB;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddIndexedDB(dbStore =>
+{
+    dbStore.DbName = "TheFactory";
+    dbStore.Version = 1;
+
+    dbStore.Stores.Add(new StoreSchema
+    {
+        Name = "Employees",
+        PrimaryKey = new IndexSpec { Name = "id", KeyPath = "id", Unique = true, Auto = true },
+        Indexes =
+                    [
+                        new IndexSpec{Name="firstName", KeyPath = "firstName", Auto=false},
+                        new IndexSpec{Name="lastName", KeyPath = "lastName", Auto=false}
+                    ]
+    });
+
+    dbStore.Stores.Add(new StoreSchema
+    {
+        Name = "Outbox",
+        PrimaryKey = new IndexSpec { Auto = true }
+    }
+    );
+});
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
